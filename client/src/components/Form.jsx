c74c8cae1abecd2ff7e6b463122e562187df793c
SirIvan02 <52300820+SirIvan02@users.noreply.github.com>
2023-09-23 11:55:11 +0200

    Update Form.jsx

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -24,7 +24,7 @@ const Form = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('https://portfolio-backend-nine-rosy.vercel.app/send-email', {
+      const response = await fetch('https://portfolio-backend-lovat.vercel.app/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
