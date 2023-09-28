c738d086e1c1028c40c7738eb194406fe6ee297b
SirIvan02 <52300820+SirIvan02@users.noreply.github.com>
2023-09-28 04:17:50 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.get('/', (req,res) => {
     res.json({message:'Ciao! sei nel backend'});
 });
 
-app.post('/send-email', (req, res) => {
+app.post('/api/send-email', (req, res) => {
   const { name, email, message } = req.body;
 
   const transporter = nodemailer.createTransport({
