49b0344e1295c79940f305fa8d10c7c4cc4a4b87
SirIvan02 <52300820+SirIvan02@users.noreply.github.com>
2023-10-06 01:51:40 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,13 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.ivancamassa.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.post('/api/send-email', (req, res) => {
   const { name, email, message } = req.body;
 
