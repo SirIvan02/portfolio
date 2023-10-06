56d97162957e6f6c57225d987fdfb9e6670378a3
SirIvan02 <52300820+SirIvan02@users.noreply.github.com>
2023-10-06 02:25:20 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json());
 
-app.post('/api/send-email', (req, res) => {
+app.post('/send-email', (req, res) => {
   const { name, email, message } = req.body;
 
   const transporter = nodemailer.createTransport({
