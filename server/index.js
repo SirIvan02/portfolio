05791f9cd613e593a315525e9a0e054462d034d6
SirIvan02 <52300820+SirIvan02@users.noreply.github.com>
2023-10-06 02:17:25 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,20 +6,7 @@ require('dotenv').config();
 
 const app = express()
 
-app.use(cors({
-  origin: 'https://www.ivancamassa.com',
-  methods: ["GET", "POST", "PUT", "DELETE"]
-  })
-);
-
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.ivancamassa.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 app.post('/api/send-email', (req, res) => {
   const { name, email, message } = req.body;
