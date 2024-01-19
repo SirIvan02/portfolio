f8b1717e89f08ff135855ad0a1f1e2a059db8b24
SirIvan02 <ivancamassa@gmail.com>
2024-01-19 17:15:09 +0100

    commit

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -24,13 +24,13 @@ const Form = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('https://portfolio-backend-lovat.vercel.app', {
+      const response = await fetch('https://portfolio-backend-lovat.vercel.app/send-email', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           },
           mode: 'no-cors',
-          body: JSON.stringify({formData}),
+          body: JSON.stringify(formData),
         });
       if (!response.ok) {
         throw new Error("Errore durante l'invio del messaggiooo");
